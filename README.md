7096ba72ce315d7d5bd64436784c7a83beb790de
Johan <johan@wtmg.se>
2023-07-22 10:59:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PHP Simple Boiler
+
+Start dev server
+```
+php -S localhost:8080
+```
+
+Run Tailwind watch on files
+```
+./tailwindcss -i ./dist/input.css -o ./dist/output.css --watch
+```
+
+Minify for production
+```
+./tailwindcss -i ./dist/input.css -o ./dist/output.css --minify
+```
+
+Roadmap:
+- [] Router
+- [] Landing Page
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP Simple Boiler</title>
+   <link href="./dist/output.css" rel="stylesheet">
+</head>
+<body>
+   <main class="max-w-screen-xl mx-auto">
+      <div class="flex flex-col items-center justify-center h-screen gap-5">
+            <h1 class="text-4xl">PHP Simple Boiler</h1>
+            <p class="text-teal-500">Powered by Tailwind</p>
+      </div>
+   </main>
+</body>
+</html>
