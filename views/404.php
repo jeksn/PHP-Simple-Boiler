4743346d095827df38a13160c484aa72d91a1a83
Johan <johan@jeksn.me>
2023-07-22 16:54:40 +0200

    change git config email and hotfix

--- views/404.php
+++ views/404.php
@@ -3,7 +3,7 @@
 
 <div class="flex flex-col items-center justify-center h-screen gap-5">
       <h1 class="text-4xl">Sorry. Page Not Found.</h1>
-      <a href="/" class="text-blue-500 underline">Go back home.</a>
+      <a href="/" class="text-blue-500 underline underline-offset-1">Go back home.</a>
 </div>
 
 <?php require('partials/footer.php') ?>
